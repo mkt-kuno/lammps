4daf97cc6b3fef2724c80dfec33e5de8f1cadeb8
Kevin Hanley <kh@cvcosulliv01.cv.ic.ac.uk>
2014-08-04 15:11:57 +0100

    Updating DumpVTK to reflect upstream changes in DumpCustom.

--- src/dump_vtk.cpp
+++ src/dump_vtk.cpp
@@ -34,7 +34,7 @@ using namespace LAMMPS_NS;
 // customize by adding keyword
 // also customize compute_atom_property.cpp
 
-enum{ID,MOL,TYPE,ELEMENT,MASS,
+enum{ID,MOL,PROC,PROCP1,TYPE,ELEMENT,MASS,
      X,Y,Z,XS,YS,ZS,XSTRI,YSTRI,ZSTRI,XU,YU,ZU,XUTRI,YUTRI,ZUTRI,
      XSU,YSU,ZSU,XSUTRI,YSUTRI,ZSUTRI,
      IX,IY,IZ,
@@ -420,6 +420,14 @@ int DumpVTK::count()
                      "Threshhold for an atom property that isn't allocated");
         int *molecule = atom->molecule;
         for (i = 0; i < nlocal; i++) dchoose[i] = molecule[i];
+        ptr = dchoose;
+        nstride = 1;
+      } else if (thresh_array[ithresh] == PROC) {
+        for (i = 0; i < nlocal; i++) dchoose[i] = me;
+        ptr = dchoose;
+        nstride = 1;
+      } else if (thresh_array[ithresh] == PROCP1) {
+        for (i = 0; i < nlocal; i++) dchoose[i] = me;
         ptr = dchoose;
         nstride = 1;
       } else if (thresh_array[ithresh] == TYPE) {
@@ -980,6 +988,12 @@ int DumpVTK::parse_fields(int narg, char **arg)
         error->all(FLERR,"Dumping an atom property that isn't allocated");
       pack_choice[i] = &DumpVTK::pack_molecule;
       vtype[i] = INT;
+    } else if (strcmp(arg[iarg],"proc") == 0) {
+      pack_choice[i] = &DumpCustom::pack_proc;
+      vtype[i] = INT;
+    } else if (strcmp(arg[iarg],"procp1") == 0) {
+      pack_choice[i] = &DumpCustom::pack_procp1;
+      vtype[i] = INT;
     } else if (strcmp(arg[iarg],"type") == 0) {
       pack_choice[i] = &DumpVTK::pack_type;
       vtype[i] = INT;
@@ -1378,6 +1392,8 @@ int DumpVTK::modify_param(int narg, char **arg)
 
     if (strcmp(arg[1],"id") == 0) thresh_array[nthresh] = ID;
     else if (strcmp(arg[1],"mol") == 0) thresh_array[nthresh] = MOL;
+    else if (strcmp(arg[1],"proc") == 0) thresh_array[nthresh] = PROC;
+    else if (strcmp(arg[1],"procp1") == 0) thresh_array[nthresh] = PROCP1;
     else if (strcmp(arg[1],"type") == 0) thresh_array[nthresh] = TYPE;
     else if (strcmp(arg[1],"mass") == 0) thresh_array[nthresh] = MASS;
 
@@ -1671,6 +1687,26 @@ void DumpVTK::pack_molecule(int n)
 
   for (int i = 0; i < nchoose; i++) {
     buf[n] = molecule[clist[i]];
+    n += size_one;
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+void DumpCustom::pack_proc(int n)
+{
+  for (int i = 0; i < nchoose; i++) {
+    buf[n] = me;
+    n += size_one;
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+void DumpCustom::pack_procp1(int n)
+{
+  for (int i = 0; i < nchoose; i++) {
+    buf[n] = me+1;
     n += size_one;
   }
 }
